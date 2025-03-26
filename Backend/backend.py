d8de259a927907c5ee9544f100067e24bdf2c9fa
Benas Siaudvytis <siaudvytisbenas@gmail.com>
2025-03-26 11:54:52 +0000

    fix

--- Backend/backend.py
+++ Backend/backend.py
@@ -387,7 +387,7 @@ def handle_connect(mqtt_client, userdata, flags, rc):
     if rc == 0:
         print("Connected to MQTT broker!:")
         mqtt.subscribe('door/responses')
-        mqtt.subscribe("door/otp/verify")
+        mqtt.subscribe('door/otp/verify')
     else:
         print("Failed to connect, return code %d\n", rc)
 
