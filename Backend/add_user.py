ce5d6f16817740b433565454499592832c664766
Benas Siaudvytis <siaudvytisbenas@gmail.com>
2025-04-09 11:51:51 +0100

    Remove sensitive files from Git tracking and update .gitignore

--- Backend/add_user.py
+++
@@ -1,8 +0,0 @@
-from backend import app, db, Admin
-
-with app.app_context():
-	new_admin = Admin(username = "Benas", phone_number="+447399963248")
-	new_admin.set_password("secret123")
-	db.session.add(new_admin)
-	db.session.commit()
-	print("Admin Benas added successfully!")
